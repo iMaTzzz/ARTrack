c28a183ec6253f8f84e2ffcd57a41f533ee8af99
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-22 17:12:36 +0100

    Added print statement to understand where to put .pth.tar file

--- lib/test/evaluation/environment.py
+++ lib/test/evaluation/environment.py
@@ -4,6 +4,7 @@ import os
 
 class EnvSettings:
     def __init__(self):
+        print(test_path)
         test_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
         self.results_path = '{}/tracking_results/'.format(test_path)
