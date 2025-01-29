2afd7349904185edd222abba4f801f0a487327c5
Matteo Sciascia <m.sciascia@fove-inc.com>
2025-01-29 10:58:08 +0900

    Removed tracking_name by default in export2onnx

--- tracking/export2onnx.py
+++ tracking/export2onnx.py
@@ -9,10 +9,9 @@ if prj_path not in sys.path:
 from lib.test.evaluation.tracker import Tracker
 
 
-def export2onnx(tracker_name, tracker_param, input_video):
+def export2onnx(tracker_param, input_video):
     """Run the tracker on a video.
     args:
-        tracker_name: Name of tracking method.
         tracker_param: Name of parameter file.
         input_video: Path to the input video (mp4)
         output: Path to the output video (mp4)
@@ -20,7 +19,7 @@ def export2onnx(tracker_name, tracker_param, input_video):
         debug: Debug level.
         save_results: Bool if we want to save the predictions
     """
-    print(f"{tracker_name=}, {tracker_param=}, {input_video=}")
+    print(f"{tracker_param=}, {input_video=}")
 
     # Initializing tracker
     tracker = Tracker("artrackv2_seq", tracker_param)
@@ -33,13 +32,12 @@ def export2onnx(tracker_name, tracker_param, input_video):
 
 def main():
     parser = argparse.ArgumentParser(description='Run the tracker on a video.')
-    parser.add_argument('tracker_name', type=str, help='Name of tracking method.')
     parser.add_argument('tracker_param', type=str, help='Name of config file.')
     parser.add_argument('input_video', type=str, help='path to the input video.')
 
     args = parser.parse_args()
 
-    export2onnx(args.tracker_name, args.tracker_param, args.input_video)
+    export2onnx(args.tracker_param, args.input_video)
 
 
 if __name__ == '__main__':
