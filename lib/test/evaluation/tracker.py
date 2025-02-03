40dceb399fd86d81c1d6b0117be6a04a05bd4c58
Matteo Sciascia <m.sciascia@fove-inc.com>
2025-02-03 15:30:13 +0900

    added input names

--- lib/test/evaluation/tracker.py
+++ lib/test/evaluation/tracker.py
@@ -304,7 +304,7 @@ class Tracker:
             input_names = ['template', 'dz_feat', 'search', 'seq_input']
             # output_names = ['seqs', 'feat', 'x_feat', 'dz_feat']
             print('\n Exporting................... \n')
-            torch.onnx.export(model=model, args=dummy_input, f=onnx_path, verbose=True, opset_version=15)
+            torch.onnx.export(model=model, args=dummy_input, f=onnx_path, verbose=True, input_names=input_names, opset_version=15)
             # torch.onnx.export(model=model, args=dummy_input, f=onnx_path, verbose=True, input_names=input_names, output_names=output_names, opset_version=15)
 
     def run_onnx(self, input_video, init_bbox, input_onnx):
